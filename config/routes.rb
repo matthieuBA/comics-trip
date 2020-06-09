d55d159148702a2d3efb3e71b5a6168a8cf21f94
matthieuBA <matthieubacheley@gmail.com>
2020-06-09 20:45:33 +0200

    modify routes for book_cards

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ devise_for :users
 resources :users, only: [:show]
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :index
+resources :book_cards
 end
