eb130c0d2a9b5ba0d2b7138eebf1fedfeea2678e
matthieuBA <matthieubacheley@gmail.com>
2020-06-09 21:39:21 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "book_cards#index"
+root to: "static_pages#home"
 devise_for :users
 resources :users, only: [:show]
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
