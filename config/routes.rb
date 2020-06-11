238660cac7a24477334c227c4ec40e225e2fb0b1
Flamislas <melvin.hersent@gmail.com>
2020-06-11 10:39:15 +0200

    edit book_card/Done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 root to: "static_pages#home"
 devise_for :users
 resources :users, only: [:show]
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :index
 resources :book_cards
 resources :books
