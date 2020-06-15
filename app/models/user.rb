69d7d21792580a9ada54f9fdcad2b2d14d9e7dda
Flamislas <melvin.hersent@gmail.com>
2020-06-15 19:00:53 +0200

    User updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
   # mailer "welcome"
   after_create :welcome_send
 
