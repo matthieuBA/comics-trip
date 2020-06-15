426b4c701a5f534cd51547114043dae682fa72f3
Flamislas <melvin.hersent@gmail.com>
2020-06-15 17:06:22 +0200

    column book_picture deleted from table book_cards

--- app/controllers/pictures_controller.rb
+++
@@ -1,7 +0,0 @@
-class PicturesController < ApplicationController
-  def create
-    @book = Book.find(params[:book_id])
-    @user.picture.attach(params[:picture])
-    redirect_to(book_path(@book))
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_085923) do
+ActiveRecord::Schema.define(version: 2020_06_15_145957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 2020_06_15_085923) do
     t.decimal "price"
     t.string "to_sell"
     t.string "book_condition"
-    t.string "book_picture"
     t.text "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
