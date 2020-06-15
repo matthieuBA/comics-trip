4a8c32d57c3c862194d041f84e8a2ff0b3748570
Flamislas <melvin.hersent@gmail.com>
2020-06-15 14:42:55 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "static_pages#home"
 devise_for :users
-resources :users, only: [:show]
-resources :index
-resources :book_cards
-resources :books
-resources :landing_pages, only: [:index]
 
+resources :users, only: [:show]
+
+resources :index
+
+resources :book_cards do
+  resources :book_pictures, only: [:create]
+end
+
+resources :books do
+  resources :pictures, only: [:create]
+end
+
+resources :landing_pages, only: [:index]
 end
