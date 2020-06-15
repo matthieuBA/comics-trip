4da47e2bb1f8ba26dc54e9fb8a9c0c3eb9a68bea
matthieuBA <matthieubacheley@gmail.com>
2020-06-15 20:40:16 +0200

    modify the seed to be more effective with the google books api

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "static_pages#home"
+  root to: "static_pages#home"
 
-devise_for :users
-resources :users, only: [:show]
+  devise_for :users
+  resources :users, only: [:show]
 
-resources :index
+  resources :index
 
-resources :books
+  resources :books
 
-resources :book_cards do
-  resources :book_pictures, only: [:create]
+  resources :book_cards do
+    resources :book_pictures, only: [:create]
+  end
+
+  resources :landing_pages, only: [:index]
+
+  resources :google_books, only: [:index]
+
+  resources :private_messages
 end
-
-resources :landing_pages, only: [:index]
-
-resources :google_books, only: [:index]
-
-resources :private_messages
-
