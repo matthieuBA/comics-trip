4f68b097b8cca8fbaf7a1e228becf10c2bd3f10d
Flamislas <melvin.hersent@gmail.com>
2020-06-15 17:25:52 +0200

    seed updated/img added for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,22 @@ Punch.destroy_all
 
 conditions = ["parfait", "moyen", "médiocre", "passable"]
 sell = ["vendable", "commenté", "recherché"]
+img = [ "/home/melvin/Documents/code/THP/travail/projet_final/comics-trip/vendor/img/jaredd-craig-HH4WBGNyltc-unsplash.jpg",
+"/home/melvin/Documents/code/THP/travail/projet_final/comics-trip/vendor/img/lena-rose-ydHrpfgJNPo-unsplash.jpg",
+"/home/melvin/Documents/code/THP/travail/projet_final/comics-trip/vendor/img/miika-laaksonen-nUL9aPgGvgM-unsplash.jpg",
+"/home/melvin/Documents/code/THP/travail/projet_final/comics-trip/vendor/img/waldemar-brandt-eIOPDU3Fkwk-unsplash.jpg"
+]
 
 50.times do |i|
   u=User.create(password:"not_blank",email:Faker::Internet.email)
+
   b=Book.create(title:Faker::DcComics.title, author:Faker::Book.author, genre:Faker::Book.genre, isbn:Faker::Code.isbn, picture:"https://www.bedetheque.com/media/Couvertures/Couv_3958"+(i+10).to_s+".jpg", abstract:Faker::Quote.yoda, extract:Faker::Quote.famous_last_words)
+  
   p "https://www.bedetheque.com/media/Couvertures/Couv_3958"+(i+10).to_s+".jpg"
-  bc=BookCard.create(user_id: User.all.sample.id, book_id: Book.all.sample.id, price: rand(100), to_sell: sell[rand(3)], book_condition: conditions[rand(3)], book_picture: "https://www.bedetheque.com/media/Couvertures/Couv_3958"+(i+10).to_s+".jpg", review:Faker::Quote.famous_last_words )
+  
+  bc=BookCard.create(user_id: User.all.sample.id, book_id: Book.all.sample.id, price: rand(100), to_sell: sell[rand(3)], book_condition: conditions[rand(3)], review:Faker::Quote.famous_last_words )
+  
+  bc.book_picture.attach(io: File.open(img.sample), filename: "book_picture.jpg", content_type: "image/jpg")
+
   p=Punch.create(punchable_id: BookCard.all.sample.id, punchable_type: "BookCard", starts_at: Time.zone.now, ends_at: Time.zone.now, average_time: Time.zone.now, hits: rand(1..10))
 end
-
