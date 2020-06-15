b1d70a85b31c98773b7231d8aebe1ad3120dbf12
cyrielle <lacyrielle@gmail.com>
2020-06-15 15:09:42 +0200

    CREATE message controller DONE, CREATE routes conversation messages DONE, CREATE views DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'conversations/index'
+  
+resources :conversations, only: [:index] do
+resources :messages, only: [:index]
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "static_pages#home"
 devise_for :users
