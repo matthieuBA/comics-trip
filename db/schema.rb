a70410132d24cb2e28a07fe6415b668a88e32249
Flamislas <melvin.hersent@gmail.com>
2020-06-16 10:37:53 +0200

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_164105) do
+ActiveRecord::Schema.define(version: 2020_06_16_083552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 2020_06_15_164105) do
     t.text "extract"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "money_pots", force: :cascade do |t|
+    t.bigint "user_id"
+    t.decimal "money"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_money_pots_on_user_id"
   end
 
   create_table "private_messages", force: :cascade do |t|
